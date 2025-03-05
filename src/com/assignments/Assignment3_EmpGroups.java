a37d2b093964929e4704c6adcd0f9b366774f91a
Ashok Tayade <ashoktayade007@gmail.com>
2025-03-05 23:15:38 +0530

    Assignment2_Arrays

---
+++ src/com/assignments/Assignment3_EmpGroups.java
@@ -0,0 +1,18 @@
+package com.assignments;
+
+public class Assignment3_EmpGroups {
+
+	public static void main(String[] args) {
+		
+       System.out.println();
+		
+		Assignment3_Employees info = new Assignment3_Employees();
+		
+		System.out.println("employee name : "+ info.employee_names1[0]+","+"employee_id : "+info.employee_Id1[0]);
+		System.out.println("employee name : "+ info.employee_names1[1]+","+"employee_id : "+info.employee_Id1[1]);
+		System.out.println("employee name : "+ info.employee_names1[2]+","+"employee_id : "+info.employee_Id1[2]);
+		
+
+	}
+
+}
---
+++ src/com/assignments/Assignment3_Employees.java
@@ -0,0 +1,20 @@
+package com.assignments;
+
+public class Assignment3_Employees {
+	
+	
+
+	String [] employee_names1={"Bharath", "ABC","XYZ"};
+	int[] employee_Id1 = {1234,1235,1236};
+
+	public static void main(String[] args) {
+		
+		System.out.println("Main method");
+		
+//		String [] employee_names1={"Bharath", "ABC","XYZ"};
+//		int[] employee_Id1 = {1234,1235,1236};
+		
+
+	}
+
+}
